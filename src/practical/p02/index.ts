b643e66b3d00f9f67e05ad90751be4d96ce03997
AsiaE14 <sadgudo@gmail.com>
2026-01-16 19:32:54 +0700

    02

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 type newUser = {
   name: string;
   username?: string;
@@ -20,4 +21,35 @@ type newUser = {
     bs: string;
   };
 };
-export function addUser(newUser: newUser | null) {}
+export async function addUser(newUser: newUser | null) {
+  try{
+    const a = await axios.get("https://jsonplaceholder.typicode.com/users");
+    const  b = a.data;
+
+
+    if(newUser){
+       const lastId = b.length > 0 ? b[b.length - 1].id : 0;
+      const newId = lastId + 1;
+
+      const add ={
+        id:newId,
+        ...newUser,
+      };
+
+      b.push(add);
+
+
+       return b.map((user: any) => ({
+      id: user.id,
+      name: user.name,
+      phone: user.phone,
+      address: user.address || null,
+    }));
+
+
+
+    }
+  }catch(e){
+    return [];
+  }
+}
