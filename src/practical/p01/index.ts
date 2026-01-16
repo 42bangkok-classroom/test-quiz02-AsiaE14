c2e826e61f6e5e4a929cecd901cd78df44264ccc
AsiaE14 <sadgudo@gmail.com>
2026-01-16 19:29:44 +0700

    01

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,7 +1,58 @@
 import axios from "axios";
-async  function getPostalAddress() {
 
+
+interface G{
+    lat:string;
+    lng:string;
+}
+interface A{
+    street:string;
+    sutie:string;
+    city:string;
+    zipcode:string;
+    geo:G|null;
     
+}
+interface User{
+    id:number;  
+    name:string;
+    username:string;
+    email: string;
+    adress:A|null;
+   
+
+}
+
+interface real{
+     id:number;
+    name:string;
+    phone:string;
+    address: A| null;
+
+}
+   
+export async  function getPostalAddress() {
+    try{
+        const a = await axios.get<real[]>("https://jsonplaceholder.typicode.com/users");
+        const data = a.data;
+        if(!data ||data.length === 0){
+            return 0;
+
+        }
+        return data.map((r) =>({
+            id:r.id,
+            name:r.name,
+            phone:r.phone,
+            address:r.address
+        }));
+
+
+    }catch(e){
+        return 0;
+    }
+
+
+
 
 
 }
